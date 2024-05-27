6261a9007acb3b2c78cca495448a5288a5ca1d7a
shitface <president@gmails>
2024-05-28 01:36:49 +0800

    password hash with bcrypt + updated dump

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,6 @@
 
 const User = require("../models/User")
-
+const bcrypt = require('bcryptjs')
 
 const addUser = async (req, res)=>{
     try{
@@ -11,8 +11,11 @@ const addUser = async (req, res)=>{
             return res.status(400).json({message: "Username is already taken."})
         } 
         
+        var salt = bcrypt.genSaltSync()
+        var hash = bcrypt.hashSync(password, salt)
+
         user = await User.create({
-            username, password
+            username, password: hash
         })
         return res.status(201).json({message: "Successfully added!"})
     } catch (err) {
@@ -20,16 +23,18 @@ const addUser = async (req, res)=>{
     }
 }
 
-const login = async (req, res)=>{ //wip
+const login = async (req, res)=>{ //wip to send jwt token?
     try{
         const {username, password} = req.body;
-        let user = await User.findOne({ username, password });
+
+        let user = await User.findOne({ username });
 
         if (user) {
-            return res.status(200).json({message: "Login successful. You have logged in as " +user})
+            if (bcrypt.compareSync(password, user.password)){
+                return res.status(200).json({message: "Login successful. You have logged in as " +user})
+            }
         } 
-        
-        return res.status(400).json({message: "Wrong username or password"})
+        return res.status(401).json({message: "Wrong username or password"})
     } catch (err) {
         return res.status(500).json({message: err.message});
     }
