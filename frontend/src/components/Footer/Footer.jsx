2c34ac547b29080b6b03b5c067c60f204e13a28c
Victor <113668641+Vict0rK@users.noreply.github.com>
2024-06-02 17:45:45 +0800

    modified navbar with the new logo

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
         <div className="footer-content">
             <div className="footer-content-left">
-                <img src={assets.logo} alt=''/>
+                <img className="footer-logo" src={assets.logo} alt=''/>
                 <p>Hello testing testing ajhsdhasjkdhajkshdjakhsdkjahsjdlhaskjdhkjashdkajshdkjahsdklah</p>
                 <div className="footer-social-icons">
                     <img src={assets.facebook_icon} alt=''/>
