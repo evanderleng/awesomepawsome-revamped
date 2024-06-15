ab4b461c6d8ac5d35d1fcd74278a9d878b0c3fac
shitface <president@gmails>
2024-06-15 09:05:05 +0800

    fixed updateCart, added validation for addUser

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 
 
 const authMiddleware = require('../middleware/authMiddleware.js')
+const validatorMiddleware = require('../middleware/validatorMiddleware.js')
 const userController = require("../controllers/userController.js");
 
 
 router.route("/editProfile").patch(authMiddleware.auth, userController.editProfile);
 router.route("/getProfile").get(authMiddleware.auth, userController.getProfile);
 
-router.post("/addUser", userController.addUser);
+router.post("/addUser",validatorMiddleware.checkAddUser, userController.addUser);
 router.post("/login", userController.login);
 
 
