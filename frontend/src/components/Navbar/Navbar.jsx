fc64faa5e68256a887557f040d9fbdeec5f7b7c4
shitface <president@gmails>
2024-06-22 18:25:33 +0800

    fixed navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,37 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom';
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = ({setShowLogin}) => {
     
     // function name is setMenu, menu is the state
-    const [menu, setMenu] = useState(""); // state to track the active menu item
-
-
-    const location = useLocation(); // hook to get the current location (current url)
-  
-    // use useEffect to update the menu state based on the current URL path
-    useEffect(() => {
-      // extract the pathname from the location object
-      const currentPath = location.pathname;
-  
-      // map paths to menu names
-      const pathToMenuMap = {
-        "/": "home",
-        "/productPage": "product",
-        "/recommendMePage": "recommend me",
-        "/contactUs": "contact us",
-        // Add more mappings as needed
-      };
-  
-      // Determine the active menu based on the current path
-      const activeMenu = pathToMenuMap[currentPath] || "home"; // Default to "home" if the path is not in the map
-      setMenu(activeMenu);
-    }, [location.pathname]); // update state whenever the location's pathname change
-
+    const [menu, setMenu] = useState("home");   // create state variable and initialise with home
 
     return(
         <div className='navbar'>
@@ -46,6 +21,8 @@ const Navbar = ({setShowLogin}) => {
                 <li onClick={() => setMenu("product")} className={menu==="product"?"active":""}><Link to="/productPage">product</Link></li>
                 <li onClick={() => setMenu("recommend me")} className={menu==="recommend me"?"active":""}><Link to="/recommendMePage">recommend me</Link></li>
                 <li onClick={() => setMenu("contact us")} className={menu==="contact us"?"active":""}>contact us</li>
+                <li onClick={() => setMenu("profile")} className={menu==="profile"?"active":""}><Link to="/profile">profile</Link></li>
+
             </ul>
             <div className='navbar-right'>
                 <div className='navbar-basket-icon'>
@@ -60,6 +37,7 @@ const Navbar = ({setShowLogin}) => {
                         </Link>
                     </li>
                 </ul>
+
                 <button onClick={() => setShowLogin(true)}>Sign In</button>
             </div>
         </div>
