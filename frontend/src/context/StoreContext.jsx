bf25780581e070e4f35bb4f409ff0fea97324749
Victor <113668641+Vict0rK@users.noreply.github.com>
2024-06-25 00:52:35 +0800

    Added isLogin state in StoredContext and updated Navbar to show buttons depending on isLogin State

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,40 +7,13 @@ export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
-    // this is to manage the cart items , how many to add
-const [cartItems, setCartItems] = useState({});
-
-const addToCart = (itemId) => {
-    // Check if the cart does not have this item with this itemId
-    if (!cartItems[itemId]) {
-        // If the item is not in the cart, add it with a quantity of 1
-        setCartItems((prev) => ({
-            ...prev,         // Spread the previous state to keep existing items in the cart
-            [itemId]: 1      // Add the new item with a quantity of 1
-        }))
-    } else {
-        // If the item is already in the cart, increase its quantity by 1
-        setCartItems((prev) => ({
-            ...prev,                        // Spread the previous state to keep existing items in the cart
-            [itemId]: prev[itemId] + 1      // Update the quantity of the existing item
-        }))
-    }
-};
-
-
-const removeFromCart = (itemId) => {
-    setCartItems((prev)=>({...prev, [itemId]: prev[itemId]-1}))  // remove quantity by 1 for that itemId
-}
-
-useEffect(()=>{console.log(cartItems);}, [cartItems])
+    const [isLogin, setIsLogin] = useState(true); // state variable to see if user is logged in or not
 
 
     const contextValue = {
         dog_food_list,  // so now food_list can be accessed anywhere
-        cartItems,
-        setCartItems,
-        addToCart,
-        removeFromCart,
+        isLogin,
+        setIsLogin
     }
     
     return(
