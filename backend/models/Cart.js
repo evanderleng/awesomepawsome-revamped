abfa17adc9eab8c5469a2e028321340f791369c3
evanderleng <2003284@sit.singaporetech.edu.sg>
2024-06-26 13:34:54 +0800

    Update Cart.js

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -4,7 +4,7 @@ const cartSchema = new mongoose.Schema({
     user_id: {type: mongoose.Schema.Types.ObjectId, ref: "User", required: true},
     cart_list: [{
         product_id: {type: mongoose.Schema.Types.ObjectId, ref:"Product", required: true},
-        quantity: {type: Number, validate:{ validator: (input) => input.isInteger(), message: "{VALUE} is not an integer" }, required: true}
+        quantity: {type: Number, required: true}
     }]
 },
 {timestamps: true}
