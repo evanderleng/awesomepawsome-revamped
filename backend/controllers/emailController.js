7d4c301ba2205fbf97b88714a43ad4f577ee7947
joeltyy <kali@kali>
2024-06-29 23:23:41 +0800

    added reset password sending

--- backend/controllers/emailController.js
+++ backend/controllers/emailController.js
@@ -1,45 +1,64 @@
+const crypto = require('crypto');
+
 const transporter = require("../services/EmailService.js")
 const User = require('../models/User.js');
 
-const sendTestEmail = async(req, res) => {
-    // Sends test email to email of your choice
-    transporter.sendMail({
-        from: '"AwesomePawsome" <${process.env.GMAIL_USER_EMAIL}>', // sender address
-        to: "receiverone@gmail.com, receivertwo@outlook.com", // list of receivers
-        subject: "Test Email from AwesomePawsome", // Subject line
-        text: "test 12345", // plain text body
-        html: "<b>test 12345</b>", // html body
-    }).then(info => {
-        return res.status(200).json({message: "Successfully sent!", contents: {info}});
-    }).catch(err => {
-        console.log(err) // remove when submitting
-        return res.status(400).json({message: "Unsuccessfully sent :(", err: {err}})
-    })
+
+const sendResetPasswordEmail = async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        
+        // Will only send email if user is present
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        const token = crypto.randomBytes(20).toString('hex');
+    
+        const resetPasswordLink = `https://awesomepawsome.shop/reset-password?token=${token}`
+
+        const emailContent = `
+            <p>Dear ${user.username},</p>
+            <p>Click here to reset your password:</p>
+            <p><a href="${resetPasswordLink}">Reset Password</a></p>
+            <p>Love,<br>AwesomePawsome</p>
+        `;
+
+        transporter.sendMail({
+            from: '"AwesomePawsome" <${process.env.GMAIL_USER_EMAIL}>', // name + sender address
+            to: email,                                                  // list of receivers
+            subject: "Password Reset Request",                          // Subject line
+            html: emailContent,                                         // html body
+        }).then(info => {
+            return res.status(200).json({ message: "Successfully sent!", contents: { info } });
+        })
+    }
+    catch (err) {  
+        console.log(err) // TODO: remove this line when submitting
+        return res.status(500).json({ message: 'Internal Error', errors:{err} });
+        // return res.status(400).json({ message: 'Internal Error'}); // TODO: uncomment this and remove above when submitting
+    }
 }
 
-const postEmail = async(req, res) => {
-    const {email} = req.body;
-    transporter.sendMail({
-        from: '"AwesomePawsome" <${process.env.GMAIL_USER_EMAIL}>', // sender address
-        to: email,//to: "receiverone@gmail.com, receivertwo@outlook.com", // list of receivers
-        subject: "Test Email from AwesomePawsome", // Subject line
-        text: "test 12345", // plain text body
-        html: "<b>test 12345</b>", // html body
-    }).then(info => {
-        return res.status(200).json({message: "Successfully sent!", contents: {info}});
-    }).catch(err => {
-        console.log(err); // remove when submitting
-        return res.status(400).json({message: "Unsuccessfully sent :(", err: {err}})
-    })
+const send2faEmail = async (req, res) =>{
+    // TODO: 2FA
+    try {
+
+    }
+    catch (err){
+
+    }
 }
 
-const verify = async(req, res) => {
+const verify = async (req, res) => {
+    // Verifies if connection to Gmail SMTP server is working
     transporter.verify().then(info => {
-        return res.status(200).json({message: {info}});
+        return res.status(200).json({ message: { info } });
     }).catch(err => {
         console.log(err) // remove when submitting
-        return res.status(400).json({err: {err}});
+        return res.status(400).json({ err: { err } });
     })
 }
 
-module.exports = {sendTestEmail, postEmail, verify};
+module.exports = { verify, sendResetPasswordEmail };
