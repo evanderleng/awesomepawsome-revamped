9cba2c256ab36bd32d226b7fe2d91662b7220d26
evanderleng <2003284@sit.singaporetech.edu.sg>
2024-07-05 14:38:35 +0800

    Update UserRoutes.js

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -15,13 +15,11 @@ router.route("/editProfile").post(authMiddleware.auth, userController.editProfil
 router.route("/getProfile").get(authMiddleware.auth, userController.getProfile);
 
 router.post("/addUser", checkAddUserReq, checkValid, userController.addUser);
-//router.post("/login", checkLoginReq, checkValid, checkOTP, userController.login);
-router.post("/login", checkLoginReq, checkValid, userController.login); //dev login, no OTP needed. email WILL still be sent
+router.post("/login", checkLoginReq, checkValid, checkOTP, userController.login);
+//router.post("/login", checkLoginReq, checkValid, userController.login); //dev login, no OTP needed. email WILL still be sent
 
 //router.post("/login_2fa", checkLoginReq, checkValid, userController.login_2fa)
 
-// router.route("/hasProduct").get(authMiddleware.auth, userController.hasProduct);
-// router.route("/hasReview").get(authMiddleware.auth, checkHasReviewReq, checkValid, userController.hasReview); //to add validation for product_id
 router.route("/canReview").get(authMiddleware.auth, checkCanReviewReq, userController.canReview);
 
 router.post("/resetPassword", checkResetPasswordTokenReq, checkValid, userController.resetPassword)
