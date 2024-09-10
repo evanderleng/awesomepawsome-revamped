054c096849a8dda9554c9930f7faa531d34adc5d
evan <president@gmails>
2024-09-11 02:41:00 +0800

    fixed rating

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -4,9 +4,7 @@ import { Link } from 'react-router-dom';
 import AverageStarRating from './AverageStarRating';
 import useAverageRating from './UseAverageRating';
 
-const FoodItem = ({ id, brand, name, weight, price, description, ingredients, petSize, imageURL }) => {
-    const { averageRating, ratingCount, error } = useAverageRating(id);
-
+const FoodItem = ({ id, brand, name, weight, price, description, ingredients, petSize, imageURL, rating, ratingCount }) => {
     return (
         <div className='food-item'>
             <div className='food-item-img-container'>
@@ -15,18 +13,15 @@ const FoodItem = ({ id, brand, name, weight, price, description, ingredients, pe
             <div className='food-item-info'>
                 <div className='food-item-name-rating'>
                     <p className='food-item-name'>{name}</p>
-                    {error ? (
-                        <p className='error-message'>{error}</p>
-                    ) : (
-                        <div className='food-item-rating'>
-                            <AverageStarRating rating={averageRating} />
-                            <p className='food-item-rating-count'>({ratingCount} reviews)</p>
-                        </div>
-                    )}
+                    
+                    <div className='food-item-rating'>
+                        <AverageStarRating rating={rating} />
+                        <p className='food-item-rating-count'>({ratingCount} reviews)</p>
+                    </div>
                 </div>
                 <p className='food-item-price'>${typeof price === 'number' ? price.toFixed(2) : '0.00'}</p>
 
-                <Link to={`/product/${id}`} state={{ id, brand, name, weight, price, averageRating, ratingCount, description, ingredients, petSize, imageURL }}>
+                <Link to={`/product/${id}`} state={{ id, brand, name, weight, price, rating, ratingCount, description, ingredients, petSize, imageURL }}>
                     <button className='button'>
                         More Info
                     </button>
